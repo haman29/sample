201e55c50514d09000b26d763dc6b658cb25591a
Kyohei Hamada <kyohei.hamada@gmail.com>
2015-03-11 21:17:15 +0900

    debug用のgem入れ devise用のUser作成

--- doorkeeper-sample/config/environments/development.rb
+++ doorkeeper-sample/config/environments/development.rb
@@ -16,9 +16,6 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  # 必要になったら
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -41,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener_web
 end

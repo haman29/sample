cd5a95f2d88114a2fec58d7365dad5c2313ea6b2
Kyohei Hamada <kyohei.hamada@gmail.com>
2015-03-12 15:31:24 +0900

    create user

--- doorkeeper-client-sample/config/routes.rb
+++ doorkeeper-client-sample/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
-    get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
-  end
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+    # get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+  # end
 end

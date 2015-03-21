0c27c464d3831750ca9f4138d727b1f927c00fdf
Kyohei Hamada <kyohei.hamada@gmail.com>
2015-03-21 12:22:09 +0900

    devide confirmation mail, reconfirmation mail using other template

--- devise-sample/app/models/user.rb
+++ devise-sample/app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+  # override Devise::Models::Confirmable#send_on_create_confirmation_instructions
+  def send_on_create_confirmation_instructions
+    generate_confirmation_token!  unless @raw_confirmation_token
+    send_devise_notification(:confirmation_on_create_instructions, @raw_confirmation_token, {})
+  end
 end
